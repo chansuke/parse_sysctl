c0dfb4197eaee622e5eb2ddf266929c92b3b79e0
chansuke <moonset20@gmail.com>
2024-07-14 00:43:25 +0900

    feat: add unit tests

--- src/extractor.rs
+++ src/extractor.rs
@@ -23,3 +23,26 @@ pub(crate) fn extract_lines(paths: SysCtlConfPaths) -> Result<Vec<Line>, ParseSy
         .collect();
     Ok(lines)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::Write;
+    use tempfile::NamedTempFile;
+
+    #[test]
+    fn test_extract_lines() {
+        let mut file = NamedTempFile::new().unwrap();
+        writeln!(file, "net.ipv4.ip_forward = 1").unwrap();
+        writeln!(file, "# net.ipv4.ip_forward = 0").unwrap();
+        writeln!(file, "; net.ipv4.ip_forward = 0").unwrap();
+        file.flush().unwrap();
+
+        let paths = SysCtlConfPaths(vec![file.path().to_path_buf()]);
+        let lines = extract_lines(paths).unwrap();
+        assert_eq!(lines.len(), 3);
+        assert_eq!(lines[0].0, "net.ipv4.ip_forward = 1");
+        assert_eq!(lines[1].0, "# net.ipv4.ip_forward = 0");
+        assert_eq!(lines[2].0, "; net.ipv4.ip_forward = 0");
+    }
+}
